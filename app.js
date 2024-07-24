cb3c872fd5ed2650c38e79b30f9181e822400623
ashish0082018 <av0082018@gmail.com>
2024-07-24 06:57:10 +0530

    make-frontend

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express=require('express')
 const app=express()
 const path=require('path')
+const fs =require('fs')
+
+
 app.set("view engine","ejs")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=>{
-    res.render("index")
+    fs.readdir(`./hisaab`,function(err,files){
+        if(err) return res.status(500).send(err)
+        res.render("index",{files: files})
+    })
+   
 })
 
-app.listen(3000)
+app.get("/create",(req,res)=>{
+    res.render("create")
+})
+app.post("/createhisaab",(req,res)=>{
+    fs.writeFile(`./hisaab/${req.body.title}`,req.body.content,function(err){
+        if(err) return res.status(500).send(err)
+            res.redirect("/")
+    })
+})
+
+app.listen(3001)
